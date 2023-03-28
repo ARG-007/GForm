b7570c041bde226e3b3f20fbb6bae260432618c2
ARG-Phantom <argironman@gmail.com>
2023-03-28 22:15:00 +0530

    NIL

--- php/test.php
+++ php/test.php
@@ -1,42 +1,41 @@
 <?php
 // use MongoDB\Client;
-require '../assets/vendor/autoload.php'; 
+// require '../assets/vendor/autoload.php'; 
 
-$mongo = new MongoDB\Client("mongodb://localhost:27017");
-echo "Successfully Connected";
-$gf = $mongo->GFormDB;
-echo "Selected GFormDB";
-$up = $gf->userProfiles;
-echo "Selected UserProfiles";
+// $mongo = new MongoDB\Client("mongodb://localhost:27017");
+// echo "Successfully Connected";
+// $gf = $mongo->GFormDB;
+// echo "Selected GFormDB";
+// $up = $gf->userProfiles;
+// echo "Selected UserProfiles";
 
-$doc = array("email"=>"phantomArg@hg.com");
-$up->insertOne($doc);
+// $doc = array("email"=>"phantomArg@hg.com");
+// $up->insertOne($doc);
 
-$cursor = $up->find();
+// $cursor = $up->find();
 
-foreach ($cursor as $document){
-	echo "{$document['_id']}\n";
-}
-// phpinfo();
+// foreach ($cursor as $document){
+// 	echo "{$document['_id']}\n";
+// }
 
-// $host="127.0.0.1";
-// $port=3306;
-// $socket="";
-// $user="root";
-// $password="root";
-// $dbname="testdb";
+$host="127.0.0.1";
+$port=3306;
+$socket="";
+$user="root";
+$password="root";
+$dbname="testdb";
 
-// $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-// 	or die ('Could not connect to the database server' . mysqli_connect_error());
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 
-// $result = $con->prepare($sqlQuery);
-// $result->execute();
-// $result->store_result();
-// $result->bind_result($res[],$res1[]);
-// $result->fetch();
+$result = $con->prepare($sqlQuery);
+$result->execute();
+$result->store_result();
+$result->bind_result($res[],$res1[]);
+$result->fetch();
 
-// echo $res;
-//$con->close();
+echo $res;
+$con->close();
 
 ?>
